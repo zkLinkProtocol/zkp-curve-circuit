824a91401c84d02f43f26f585aa7840022a70988
RequiemOfSouls <RequiemOfSouls@zklink.org>
2021-09-02 22:40:47 +0800

    Rewrite Curve V3 with Rust.

--- src/exchanges.rs
+++ src/exchanges.rs
@@ -6,25 +6,25 @@ use crate::{Address, N, Balance, PRECISION_MUL, ZERO};
 use crate::invariant::calculate;
 use primitive_types::U256;
 
-/// The token being withdrawn balance after the swap.
-pub fn after(
-    tokens: [Address; N],
+/// The token being withdrawn balance get_y the swap.
+pub fn get_y(
     mut balances: [Balance; N],
     amplifier: u64,
+    d: U256,
     token_x_idx: usize,
-    token_y_idx: usize,
-    after_x: Balance,
+    token_y_idx: Option<usize>,
+    after_x: Option<Balance>,
+    is_get_y_d: bool
 ) -> Balance {
+    assert!((is_get_y_d && token_y_idx.is_none()) || (!is_get_y_d && token_y_idx.is_some()));
     assert_ne!(token_x_idx, token_y_idx, "Cannot exchange between the same coins");
     assert!(0 <= token_x_idx && token_x_idx < N, "There is no x token Id in the pool");
-    assert!(0 <= token_y_idx && token_y_idx < N, "There is no y token Id in the pool");
+    assert!(0 <= token_y_idx.unwrap() && token_y_idx.unwrap() < N, "There is no y token Id in the pool");
+    
+    //  
+    balances.iter_mut().for_each(|balance| *balance *= PRECISION_MUL);
 
-    for i in 0..N {
-        balances[i] *= tokens[i].magnitude_diff() * PRECISION_MUL;
-    }
-
-    let d = calculate(balances, amplifier);
-    let an: u64 = amplifier * (N as u64);
+    let an: U256 = (amplifier * N as u64).into();
 
     // let x_magnitude_diff = tokens[token_x_idx].magnitude_diff() * PRECISION_MUL;
     // let y_magnitude_diff = tokens[token_y_idx].magnitude_diff() * PRECISION_MUL;
@@ -34,137 +34,131 @@ pub fn after(
     // let after_x_p = after_x * x_magnitude_diff;
 
     for i in 0..N {
-        let after_x_p = if i == token_x_idx {
-            after_x
-        } else if i != token_y_idx {
-            balances[i]
-        } else { continue };
+        let after_x_p = if is_get_y_d{
+            if i != token_x_idx {
+                balances[i]
+            } else { continue; }
+        } else {
+            if i == token_x_idx {
+                after_x
+            } else if i != token_y_idx.unwrap() {
+                balances[i]
+            } else { continue; }
+        };
         s += after_x_p;
         c = c * d / (after_x_p * N)
     }
 
-    c = c * d / (U256::from(an) * N);
+    c = c * d / (an * N);
 
     let b: Balance = s + d / an;
     let mut y = d;
-    let mut y_next = y;
-    let mut n = 0;
-    for _ in 0..15{
-        y_next = (y * y + c) / (2 * y + b - d);
+    for _ in 0..255{
+        let y_next = (y * y + c) / (2 * y + b - d);
 
-        if (y > y_next && y - y_next > y_magnitude_diff)
-            || (y <= y_next && y_next - y > y_magnitude_diff) {
-            y = y_next;
-        } else {
-            break;
+        if (y > y_next && y - y_next > U256::one())
+            || (y <= y_next && y_next - y > U256::one()) {
+            return y_next;
         }
-        n += 1;
-    };
+        y = y_next;
+    }
 
-    // y / y_magnitude_diff
     y
 }
 
 #[test]
 fn ok_equal_precision() {
-    let new_y = after(
-        [Address::USDT, Address::USDC],
+    let new_y = get_y(
         [1_000 as Balance, 1_000 as Balance],
         100 as u64,
-
         0,
-        1,
+        Some(1),
         1_050 as Balance,
+        false,
     );
 
-    assert_eq!(new_y, 950, "The balance after withdrawal does not match the reference");
+    assert_eq!(new_y, 950, "The balance get_y withdrawal does not match the reference");
 }
 
 #[test]
 fn ok_equal_precision_amplified() {
-    let new_y = after(
-        [Address::USDT, Address::USDC],
+    let new_y = get_y(
         [1_E6 as Balance, 1_E6 as Balance],
         100 as u64,
-
         0,
-        1,
+        Some(1),
         1_900_000 as Balance,
+        false,
     );
 
-    assert_eq!(new_y, 130_370, "The balance after withdrawal does not match the reference");
+    assert_eq!(new_y, 130_370, "The balance get_y withdrawal does not match the reference");
 }
 
 #[test]
 fn ok_different_precision_lesser_bigger() {
-    let new_y = after(
-        [Address::USDT, Address::TUSD],
+    let new_y = get_y(
         [1_E6 as Balance, 1_E18 as Balance],
         100 as u64,
-
         0,
-        1,
+        Some(1),
         1.050_E6 as Balance,
+        false,
     );
 
-    assert_eq!(new_y, 950_024_800_946_586_013, "The balance after withdrawal does not match the reference");
+    assert_eq!(new_y, 950_024_800_946_586_013, "The balance get_y withdrawal does not match the reference");
 }
 
 #[test]
 fn ok_different_precision_lesser_bigger_amplified() {
-    let new_y = after(
-        [Address::USDT, Address::TUSD],
+    let new_y = get_y(
         [1_E6 as Balance, 1_E18 as Balance],
         100 as u64,
-
         0,
-        1,
+        Some(1),
         1.950_E6 as Balance,
+        false,
     );
 
-    assert_eq!(new_y, 94_351_900_636_131_207, "The balance after withdrawal does not match the reference");
+    assert_eq!(new_y, 94_351_900_636_131_207, "The balance get_y withdrawal does not match the reference");
 }
 
 #[test]
 fn ok_different_precision_bigger_lesser() {
-    let new_y = after(
-        [Address::TUSD, Address::USDT],
+    let new_y = get_y(
         [1_E18 as Balance, 1_E6 as Balance],
         100 as u64,
-
         0,
-        1,
+        Some(1),
         1.050_E18 as Balance,
+        false,
     );
 
-    assert_eq!(new_y, 950_024, "The balance after withdrawal does not match the reference");
+    assert_eq!(new_y, 950_024, "The balance get_y withdrawal does not match the reference");
 }
 
 #[test]
 fn ok_different_precision_bigger_lesser_amplified() {
-    let new_y = after(
-        [Address::TUSD, Address::USDT],
+    let new_y = get_y(
         [1_E18 as Balance, 1_E6 as Balance],
         100 as u64,
-
         0,
-        1,
+        Some(1),
         1.950_E18 as Balance,
+        false,
     );
 
-    assert_eq!(new_y, 94_351, "The balance after withdrawal does not match the reference");
+    assert_eq!(new_y, 94_351, "The balance get_y withdrawal does not match the reference");
 }
 
 #[test]
 #[should_panic]
 fn error_same_tokens() {
-    after(
-        [Address::USDT, Address::USDC],
+    get_y(
         [1E3 as Balance, 1E3 as Balance],
         100 as u64,
-
-        1,
-        1,
+        Some(1),
+        Some(1),
         100 as Balance,
+        false,
     );
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ pub mod exchanges;
 pub mod invariant;
 
 use primitive_types::{U256,H160};
+use crate::invariant::calculate;
+use crate::exchanges::get_y;
 
 // The ETH address in the unsigned integer form.
 pub type Balance = U256;
@@ -12,11 +14,22 @@ pub const ZERO: Balance = U256::from(0);
 // The default computation precision.
 // Balances are multiplied by it during invariant computation,
 // to avoid division of integers of the same order of magnitude.
-pub const PRECISION_MUL: U256 = U256::from(1_000_000);
+pub const PRECISION_MUL: [U256; N] = [U256::from(1_000_000);N];
 // The maximum known token precision.
 pub const MAX_TOKEN_PRECISION: u8 = 18;
+
+// These constants must be set prior to compiling
+
 // The number of tokens being traded in the pool.
-pub const N: usize = 2;
+const N: usize = 2;
+const RATES:[U256;N] = [U256::default();N];
+
+// fixed constants
+const FEE_DENOMINATOR:U256 = U256::from(10).pow(10.into());
+const PRECISION:U256 = U256::from(10).pow(18.into());
+
+const ADMIN_FEE:U256 = U256::from(10).pow(10.into());
+const FEE:U256 = U256::from(10).pow(10.into());
 
 struct StableSwap{
     // The tokens being traded in the pool.
@@ -24,7 +37,9 @@ struct StableSwap{
     // The Curve amplifier.
     amplifier: u64,
     // The balances of tokens
-    balances: [Balance; N]
+    balances: [Balance; N],
+    // Total supply liquidity in the pool.
+    total_supply: U256
 }
 
 // The Curve StableSwap.
@@ -41,38 +56,97 @@ impl StableSwap {
         Self {
             tokens,
             amplifier,
-            balances
+            balances,
+            total_supply: Default::default()
         }
     }
 
-    /// Adds liquidity to the contract balances.
-    pub fn add_liquidity(&mut self, amount: U256, min_amounts: [U256;N]) -> [U256;N] {
+    fn xp(&self) -> [U256;N]{
+        let mut res = RATES;
+        res.iter_mut().enumerate().for_each(|(i,res)|*res= res* self.balances[i]/PRECISION);
+        res
+    }
 
-        // panics if the token with address `zksync::msg.token_address` is not traded in this pool
-        let deposit_idx = self.token_position(Address::from_address(zksync::msg.token_address));
+    fn get_current_d(&self) -> U256{
+        calculate(self.balances, self.amplifier)
+    }
+
+    /// Adds liquidity to the contract balances.
+    pub fn add_liquidity(&mut self, amount:[U256;N], min_lp_quantity: [U256;N]) -> U256 {
+        let amp = self.amplifier;
+        let old_balances = self.balances;
+
+        let d0 = self.get_current_d();
+        let total_supply = self.total_supply;
+        let mut new_balances = old_balances;
+        for i in 0..N {
+            if total_supply == 0 { assert!(amount[i] > 0)}
+            new_balances[i] += amount[i];
+        }
+
+        let d1 = calculate(new_balances, amp);
+        assert!(d1 > d0);
+
+        let mut d2 = d1;
+        let mut fees = [Balance::default();N];
+        let mut lp_quantity = Balance::default();
+        if total_supply > U256::zero(){
+            let fee = FEE * N / (4 * (N - 1));
+            for (i, new_balance) in new_balances.iter_mut().enumerate() {
+                let ideal_balance = d1 *old_balances[i] / d0;
+                let difference = if ideal_balance > new_balance{
+                    ideal_balance - new_balance
+                } else {
+                    new_balance - ideal_balance
+                };
+                fees[i] = fee * difference / FEE_DENOMINATOR;
+                self.balances[i] = new_balance - (fees[i] * admin_fee / FEE_DENOMINATOR);
+                new_balance[i] -= fees[i];
+            }
+            d2 = calculate(new_balances, amp);
+            lp_quantity = total_supply * (d2 - d0) / d0;
+        } else {
+            self.balances = new_balances;
+            lp_quantity = d1;
+        }
+        assert!(mint_amount >= min_lp_quantity, "Slippage screwed you");
+
+        lp_quantity
     }
 
     /// Removes liquidity to the contract balances.
     pub fn remove_liquidity(&mut self, amount: U256, min_amounts: [U256;N]) -> [U256;N]{
+        let total_supply = self.total_supply;
+        let mut amounts = [Balance::default();N];
 
-        // panics if the token with address `zksync::msg.token_address` is not traded in this pool
-        let deposit_idx = self.token_position(Address::from_address(zksync::msg.token_address));
+        for (i, balance) in self.balances.iter_mut().enumerate() {
+            let value = *balance * amount / total_supply;
+            assert!(value >= min_amounts[i], "Withdrawal resulted in fewer coins than expected");
+            *balance = balance - value;
+            amounts[i] = value;
+        }
+        amounts
+
     }
 
-    ///
+    /// Removes liquidity to the contract balances and withdraw one coins.
+    pub fn remove_liquidity_one_coin(&mut self, amount: U256, token_index: usize, min_amount: U256) -> U256{
+        let (dy, dy_fee, _total_supply) = self.calc_withdraw_coin_lp_by_removing_lp(amount, token_index);
+        assert!(dy >= min_amount, "Not enough coins removed");
+
+        self.balances[i] -= (dy + dy_fee * ADMIN_FEE / FEE_DENOMINATOR);
+        dy
+
+    }
+
     /// Exchanges the tokens, consuming some of the `zksync::msg.token_address` and returning
-    /// some of the `withdraw_token_address` to the client.
-    ///
+    /// some of the `withdraw_token_address`.
     pub fn swap(
-        mut self,
+        &mut self,
         withdraw_address: Address,
         withdraw_token_address: Address,
         min_withdraw: Balance,
     ) {
-        assert!(
-            zksync::msg.recipient == self.address,
-            "Transaction recipient is not the contract",
-        );
 
         let deposit_idx = self.token_position(Address::from_address(zksync::msg.token_address));
         let withdraw_idx = self.token_position(withdraw_token_address);
@@ -83,20 +157,19 @@ impl StableSwap {
         assert_ne!(balance_array[withdraw_idx], 0, "Withdraw token balance is zero");
 
         let new_x = balance_array[deposit_idx] + zksync::msg.amount;
-        let new_y = exchanges::after(
-            self.tokens,
+        let new_y = exchanges::get_y(
             balance_array,
             self.amplifier,
+            calculate(balance_array, self.amplifier),
             deposit_idx,
-            withdraw_idx,
-            new_x,
+            Some(withdraw_idx),
+            Some(new_x),
+            false,
         );
 
         let old_y = balance_array[withdraw_idx];
-        assert!(
-            old_y >= min_withdraw + new_y,
-            "Exchange resulted in fewer coins than expected",
-        );
+        assert!(old_y >= min_withdraw + new_y,
+            "Exchange resulted in fewer coins than expected");
         let withdraw_amount = old_y - new_y;
 
         self.transfer(
@@ -121,15 +194,16 @@ impl StableSwap {
         assert_ne!(self.balances[deposit_idx], 0, "Deposit token balance is zero");
         assert_ne!(self.balances[withdraw_idx], 0, "Withdraw token balance is zero");
 
-        let after_withdrawal = balance_array[withdraw_idx] - to_withdraw;
+        let after_withdrawal = self.balances[withdraw_idx] - to_withdraw;
 
-        let after_deposit = exchanges::after(
-            self.tokens,
+        let after_deposit = exchanges::get_y(
             balance_array,
             self.amplifier,
+            calculate(balance_array, self.amplifier),
             withdraw_idx,
-            deposit_idx,
-            after_withdrawal,
+            Some(deposit_idx),
+            Some(after_withdrawal),
+            false,
         );
 
         after_deposit - balance_array[deposit_idx]
@@ -150,23 +224,67 @@ impl StableSwap {
         assert_ne!(balance_array[deposit_idx], 0, "Deposit token balance is zero");
         assert_ne!(balance_array[withdraw_idx], 0, "Withdraw token balance is zero");
 
-        let after_deposit = balance_array[deposit_idx] + to_deposit;
+        let after_deposit = self.balances[deposit_idx] + to_deposit;
 
-        let after_withdrawal = exchanges::after(
-            self.tokens,
+        let after_withdrawal = exchanges::get_y(
             self.balances,
             self.amplifier,
+            calculate(self.balances, self.amplifier),
             deposit_idx,
-            withdraw_idx,
-            after_deposit,
+            Some(withdraw_idx),
+            Some(after_deposit),
+            false,
         );
 
         balance_array[withdraw_idx] - after_withdrawal
     }
 
-    ///
+    fn calc_removed_lp_by_withdraw_coins(&self, coins_amounts: [U256;N], max_burn_amount:U256) -> U256{
+
+    }
+
+    fn calc_withdraw_coin_lp_by_removing_lp(&self, coin_amount: U256, token_index: usize) -> (U256,U256,U256){
+        let amp = self.amplifier;
+        let xp = self.xp();
+        let d0 = calculate(xp, amp);
+
+        let total_supply = self.total_supply;
+        let d1 = d0 - coin_amount * d0 / total_supply;
+
+        let new_y = get_y(
+            xp,
+            amp,
+            d1,
+            token_index,
+            None,
+            None
+            true
+        );
+        let mut xp_reduced = xp;
+        let fee = FEE * N / (4 * (N - 1));
+        for j in 0..N {
+            let dx_expected = if j == token_index {
+                xp[j] * d1 / d0 - new_y
+            } else {
+                xp[j] - xp[j] * d1 / d0
+            };
+            xp_reduced[j] -= fee * dx_expected / FEE_DENOMINATOR;
+        }
+        let mut dy = xp_reduced[token_index] - get_y(
+            xp_reduced,
+            amp,
+            d1,
+            token_index,
+            None,
+            None,
+            true
+        );
+        dy = (dy - 1) / PRECISION_MUL[i];
+        let dy_fee = (xp[i] - new_y) / PRECISION_MUL[i];
+        (dy, dy_fee, total_supply)
+    }
+
     /// Given a token ID, returns the token position in the array of balances.
-    ///
     fn token_position(
         &self,
         token_address: Address,
